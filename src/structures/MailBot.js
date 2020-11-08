44ee92839e536a56f388df78e07e0b9203ee11ed
angel <angelbecerrah548@gmail.com>
2020-11-07 20:14:42 -0600

    feat(*): add more to client

--- src/structures/MailBot.js
+++ src/structures/MailBot.js
@@ -1,17 +1,32 @@
 const { Client } = require('discord.js');
-const { PREFIX, MONGO_URL } = require('../../configs');
+const { PREFIX, MONGO_URL, OWNERS, STAFFSERVER } = require('../../configs');
 const { connect, connection } = require('mongoose');
 const Registrator = require('../utils/Registrator');
+const serverSchema = require('../models/serverSchema');
 
 class MailBot extends Client {
   constructor(options = {}) {
     super(options);
 
     this.Registrator = new Registrator(this);
+
+    this.currentlyOpenTickets = new Map();
   }
 
   get prefix() {
     return PREFIX || '!';
+  }
+
+  get owners() {
+    return OWNERS;
+  }
+
+  get staffServer() {
+    return this.guilds.fetch(STAFFSERVER);
+  }
+
+  async database() {
+    return await this._getMaster();
   }
 
   ready(token) {
@@ -19,12 +34,24 @@ class MailBot extends Client {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    connection.on('connected', () => {
+    connection.on('connected', async () => {
       console.log('DB connected');
+      await this._getMaster();
     });
     this.Registrator.loadCommands('../commands');
     this.Registrator.loadEvents('../events');
     super.login(token);
+  }
+
+  /** @private */
+  async _getMaster() {
+    const master = await serverSchema.findOne({ main: 'master' });
+    if (!master) {
+      const masterCreated = new serverSchema();
+      await masterCreated.save();
+      return masterCreated;
+    }
+    return master;
   }
 }
 
